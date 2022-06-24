e3ab38ebe215e10a09b21eceb2b450f97b7e4b23
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-06-24 10:13:06 +0500

    Calculator Ready to Use

--- Index.js
+++ Index.js
@@ -1,2 +1,39 @@
-let X='2^2^2';
-console.log(eval(X.replaceAll('^' , '**')));
+let AllBox=document.querySelectorAll('.B');
+
+AllBox.forEach(element => 
+{
+    element.addEventListener('click' , ()=>
+    {
+        if(element.innerHTML==='C')
+        {
+            document.querySelector('.Inp').value='';    
+        }
+        else if(element.innerHTML==='=')
+        {
+            try
+            {
+                let Exp=document.querySelector('.Inp').value;
+                if(Exp.search('^')>=0)
+                {
+                    Exp=Exp.replaceAll('^' , '**');
+                }
+                let Ans=eval(Exp);
+                document.querySelector('.Inp').value=Ans;
+            }
+            catch
+            {
+                document.querySelector('.Inp').value="Expression Wrong";
+            }
+        }
+        else
+        {
+            document.querySelector('.Inp').value+=element.innerHTML;
+        }
+    });
+});
+
+document.querySelector('.form-check-input').addEventListener('click' , ()=>
+{
+    document.querySelector('.Main').classList.toggle('Dark');
+    document.querySelector('body').classList.toggle('Body-Dark');
+});
